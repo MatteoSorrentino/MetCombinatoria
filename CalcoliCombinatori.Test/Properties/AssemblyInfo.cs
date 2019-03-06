551f78302ae18db79d8e87485c5fd26a12050a8e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-06 09:08:45 +0100

    creazione di test per prova del fattoriale

---
+++ CalcoliCombinatori.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcoliCombinatori.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcoliCombinatori.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d58a70f-13f6-4371-8951-8637504784c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalcoliCombinatori.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcoliCombinatori.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(0,1)]
+        [DataRow(1,1)]
+        [DataRow(5,120)]
+        [DataRow(10, 3628800)]
+        [DataRow(20, 2432902008176640000)]
+        public void TestFattoriale(int num, int fattorialeAspettato)
+        {
+            int fattoriale = EquazioniLibrary.CalcoliCombinatori.Fattoriale(num);
+            Assert.AreEqual(fattorialeAspettato, fattoriale);
+        }
+    }
+}
